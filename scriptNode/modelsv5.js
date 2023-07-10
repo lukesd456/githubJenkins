c5ea594e8dd20980696e3972bf1a6d881c9c78f9
lukesd456 <121051835+lukesd456@users.noreply.github.com>
2023-07-10 13:31:48 -0500

    all

--- scriptNode/modelsv5.js
+++ scriptNode/modelsv5.js
@@ -16,6 +16,19 @@ const filterXPATH = (targets, type) => {
     return result
 }
 
+const generarPalabraAleatoria = (longitud) => {
+    const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+    let palabra = '';
+  
+    for (let i = 0; i < longitud; i++) {
+      const indice = Math.floor(Math.random() * alfabeto.length);
+      const letra = alfabeto.charAt(indice);
+      palabra += letra;
+    }
+  
+    return palabra;
+}
+
 const generarNumeroAleatorio = (longitud) => {
     let numero = '';
     
@@ -29,12 +42,10 @@ const generarNumeroAleatorio = (longitud) => {
 
 const filterTests = (rutina, indice, value, tipoDeTest,mensajeEsperado) => {
 
-    let copyRoutine = rutina
-    copyRoutine[indice].value = value
-    console.log(mensajeEsperado)
+    rutina[indice].value = value
 
     return {
-        rutina:copyRoutine,
+        rutina,
         indice,
         tipoDeTest,
         mensajeEsperado: mensajeEsperado
@@ -49,150 +60,194 @@ const createTests = async (path) => {
 
     file = new FilterSide(file)
 
+    let filterCommands = []
+
+    file.commands = file.commands.map(f => f.command === 'type' ? filterCommands.push(f) : f.command === 'click' && filterCommands.push(f))
+
+    file.commands = filterCommands
+
+    //Filter targets
     file.commands = file.commands.map((e) => {
         let objetivos = e.targets
 
-        let command = e.command
-        let comment = e.comment
-        let value = e.value
-
-        //Filtrar los objetivos
-
         objetivos = objetivos.map((o) => {
-
             let location = o[0]
             let detail = o[1]
-            let typeTarget
+
+            let isTarget = false
 
             if (location.includes('xpath=')) {
-
                 location = location.split('xpath=')[1]
                 detail = detail.split(':')[1]
-                typeTarget = 'xpath'
-
+                isTarget = true
             } else if (location.includes('css=')) {
-
                 location = location.split('css=')[1]
-                detail = detail.split(':')[1]
-                typeTarget = 'css'
-
-            } else {
-
-                location = location
-                detail = detail
-                typeTarget = 'none'
-
+                detail = 'css'
+                isTarget = true
             }
-            
+
             return {
                 location,
                 detail,
-                typeTarget,
-                value
+                isTarget
             }
 
         })
 
-        //Definir la ruta
+        //Quitamos los tipos de target que no interesan
+        objetivos = objetivos.filter(o => o.isTarget)
 
-        let cssTarget = objetivos.filter(e=>e.typeTarget == 'css')[0]
+        e["targets"] = objetivos
 
-        let xpathTargets = objetivos.filter(e=>e.typeTarget === 'xpath' && e)
-        
-        let location
+        return e
+    })
 
-        if (xpathTargets.length>0) {
-            let attributes = filterXPATH(xpathTargets,'attributes')
-            let idRelative = filterXPATH(xpathTargets, 'idRelative')
-            let position = filterXPATH(xpathTargets, 'position')
-            let innerText = filterXPATH(xpathTargets, 'innerText')
+    //Establecemos el objetivo
+    file.commands = file.commands.map((e) => {
+        let objetivos = e.targets
 
-            location = idRelative ? idRelative : attributes ? attributes : position ? position : innerText
+        let target
 
-        } else {
-            location = cssTarget
-        }
+        objetivos.map((o) => {
 
-        return {
-            ...location,
-            command,
-            comment
-        }
+            let data = {
+                location : o.location,
+                detail : o.detail
+            }
+
+            target && o.detail === 'idRelative' ? target = data : o.detail === 'attributes'  ? target = data : o.detail === 'position' ? target = data : o.detail === 'innerText' ? target = data : target = data
+        })
+
+        e["target"] = target
+
+        return e
 
     })
+
+    let testsToDo = []
+    let indice = 0
 
     file.commands = file.commands.map((e) => {
-        let comment = e.comment
-        let indice = file.commands.indexOf(e)
         
-        //filtrar los tests
+        let testToDo = e.comment !== '' ? true : false
+        let validador = false
+        let mensajesEsperados = []
+        
+        //Forma de documentar los detalles
+        
+        //tipoDeDato:string
+        //obligatorio:si
+        //longitud:10 o indefinida
+        //mensajesEsperados:Campo requerido.|El RUC
 
-        comment = comment.split('-').filter(c=>c!='')
+        let testNumero = false
+        let testLongitud = false
+        let testObligatorio = false
+        let longitud = 20
 
-        let testToDo = comment.length > 0 ? true : false
+        if (testToDo) {
 
-        e['validador'] = false
+            let separateComments = e.comment.split('-')
 
-        testToDo && comment.map((c) => {
-            let element = c.split(':')
+            testNumero = e.comment.includes('numero')
+            
+            testObligatorio = e.comment.includes('obligatorio')
 
-            switch (element[0]) {
-                case 'tipoDeDato':
-                    e['tipoDeDato'] = element[1]
-                    break
-                case 'obligatorio':
-                    e['obligatorio'] = element[1] == 'si' ? true:false
-                    break
-                case 'longitud':
-                    e['longitud'] = element[1]
-                    break
-                case 'mensajeEsperado':
-                    e['mensajeEsperado'] = element[1].split('|')
-                    break
-                case 'validador':
-                    e['validador'] = element[1] == 'si' && true
-                    testToDo = false
-            }
+            testLongitud = !e.comment.includes('longitud:indefinida')
+            
+            validador = e.comment.includes('validador')
 
-        })
+            let foundMessages = e.comment.includes('mensajesEsperados:')
 
-        return {
-            ...e,
-            indice,
-            testToDo
+            testLongitud && separateComments.map((c) => {
+                
+                if (c.includes('longitud:')) {
+                    
+                    //Si se encuentra longitud la establece
+                    longitud = parseInt(c.split(':')[1])
+                
+                }
+            })
+
+            foundMessages && separateComments.map((c) => {
+                if (c.includes('mensajesEsperados:')) {
+                    
+                    //Si se encuentran mensajes los establecemos
+                    mensajesEsperados = c.split(':')[1].split('|')
+                
+                }
+            })
+
+
+
         }
 
+        e["indice"] = indice
+        e["validador"] = validador
+        e["mensajesEsperados"] = mensajesEsperados
+        e["testLongitud"] = testLongitud
+        e["testObligatorio"] = testObligatorio
+        e["testNumero"] = testNumero
+        e["longitud"] = e.command === 'type' && longitud
+        
+        testToDo && testsToDo.push(e)
 
+        indice += 1
+
+        return e
     })
 
-    let testsToDo = file.commands.filter(e => e.testToDo == true)
-
-    let tests = []
+    file.tests = []
 
     testsToDo.map((t) => {
 
-        let copy = file.commands
         let indice = t.indice
-        let mensajeEsperado = t.mensajeEsperado
+        let copyArray = file.commands.slice()
+        let mensajesEsperados = t.mensajesEsperados
 
-        t.tipoDeDato != 'string' && tests.push(filterTests(copy, indice,uuidV4() + uuidV4(), 'tipoDeDato', mensajeEsperado))
+        if (t.testLongitud) {
+            copyArray.value = generarNumeroAleatorio(t.longitud)
+            file.tests.push({
+                indice,
+                mensajesEsperados,
+                tipoDeTest: 'longitud',
+                rutina: copyArray,
+            })    
+        }
 
-        t.longitud != 'indefinida' && tests.push(filterTests(copy, indice, generarNumeroAleatorio(parseInt(t.longitud)+1), 'longitud', mensajeEsperado))
+        if (t.testNumero) {
+            copyArray.value = generarPalabraAleatoria(t.longitud)
+            file.tests.push({
+                indice,
+                mensajesEsperados,
+                tipoDeTest: 'tipoDeDato',
+                rutina: copyArray
+            })
+        }
 
-        t.obligatorio && tests.push(filterTests(copy, indice, '', 'obligatoriedad', mensajeEsperado))
+        if (t.testObligatorio) {
+            copyArray.value = ''
+            file.tests.push({
+                indice,
+                mensajesEsperados,
+                tipoDeTest: 'obligatorio',
+                rutina: copyArray
+            })
+        }
 
     })
+
+    file.tests.map(t => t.rutina = t.rutina.filter(e => e.target))
 
     let finalObject = {
         targetURL : file.url,
         rutinaNormal : file.commands,
-        tests
+        tests: file.tests
     }
 
     finalObject = JSON.stringify(finalObject)
 
     writeFile('./filteredTests.json', finalObject, 'utf-8')
-
 }
 
 createTests('./testAcopio2.side')
